92b820acdef5c0ad5759f314de0dffb87e4fb48b
TienDM <TienDM@TienDM>
2017-11-20 01:21:25 +0700

    xxxx

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,3 +1,19 @@
 public class BruteCollinearPoints {
+	private Point[] points;
+	private LineSegment[] segments;
+	public BruteCollinearPoints(Point[] points) { // finds all line segments containing 4 points
+		this.points = points;
+	}
 
+	public int numberOfSegments() { // the number of line segments
+		
+	}
+
+	public LineSegment[] segments() { // the line segments
+		for(int i = 0 ; i < points.length; i++){
+			for(int j = 0; j < points.length; j++){
+				
+			}
+		}
+	}
 }
--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,4 +1,21 @@
-
 public class FastCollinearPoints {
+	private Point[] points;
+	private LineSegment[] segments;
 
+	public FastCollinearPoints(Point[] points) { // finds all line segments
+													// containing 4 points
+		this.points = points;
+	}
+
+	public int numberOfSegments() { // the number of line segments
+
+	}
+
+	public LineSegment[] segments() { // the line segments
+		for (int i = 0; i < points.length; i++) {
+			for (int j = 0; j < points.length; j++) {
+
+			}
+		}
+	}
 }
